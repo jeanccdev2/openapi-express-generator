110ec615b420da186ccf1a52df73263f73e395a9
Jean Carlos <jean.cristovao@sankhya.com.br>
2026-01-07 17:56:05 -0300

    add route generation command with OpenAPI file processing

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 #!/usr/bin/env node
 
 import { createProject } from "./commands/create.js";
+import { generateRoutes } from "./commands/generate.js";
 
 const args = process.argv.slice(2);
 
 if (args[0] === "create") {
   createProject();
+} else if (args[0] === "generate") {
+  generateRoutes();
 } else {
   console.log(`
 Uso:
