5f0fc04747a5bff31dcdf3ac4289230d3d302686
Jean Carlos <jean.cristovao@sankhya.com.br>
2026-01-09 08:09:04 -0300

    refactor file copying to use recursive directory reading and simplify path handling

--- src/flows/create.flow.ts
+++ src/flows/create.flow.ts
@@ -1,125 +1,52 @@
 import type { ORM } from "@/types/orm.js";
-import fs from "fs";
+import fs, { Dirent } from "fs";
 import path from "path";
-
-type FileFolder = {
-  name: string;
-  type: "file" | "folder";
-  path: string[];
-};
 
 const appExamplePath = path.join(process.cwd(), "app_example");
 const appGeneratedPath = path.join(process.cwd(), "app_generated");
-const defaultFiles: FileFolder[] = [
-  {
-    name: "package.json",
-    type: "file",
-    path: ["/"],
-  },
-  {
-    name: ".env",
-    type: "file",
-    path: ["/"],
-  },
-  {
-    name: ".gitignore",
-    type: "file",
-    path: ["/"],
-  },
-  {
-    name: ".prettierignore",
-    type: "file",
-    path: ["/"],
-  },
-  {
-    name: ".prettierrc",
-    type: "file",
-    path: ["/"],
-  },
-  {
-    name: "eslint.config.js",
-    type: "file",
-    path: ["/"],
-  },
-  {
-    name: "package-lock.json",
-    type: "file",
-    path: ["/"],
-  },
-  {
-    name: "tsconfig.json",
-    type: "file",
-    path: ["/"],
-  },
-];
 
 export async function createFullProjectFlow(orm: ORM) {
-  const files: FileFolder[] = [
-    ...defaultFiles,
+  const dirFiles = fs
+    .readdirSync(appExamplePath, {
+      recursive: true,
+      withFileTypes: true,
+    })
+    .filter((dirent) => {
+      const isDatabaseDir = dirent.parentPath.includes("database");
+      const isEntitiesDir = dirent.parentPath.includes("entities");
 
-    {
-      name: "config",
-      type: "folder",
-      path: ["src"],
-    },
-    {
-      name: "helpers",
-      type: "folder",
-      path: ["src"],
-    },
-    {
-      name: "shared",
-      type: "folder",
-      path: ["src"],
-    },
-    {
-      name: "types",
-      type: "folder",
-      path: ["src"],
-    },
-    {
-      name: "main.ts",
-      type: "file",
-      path: ["src"],
-    },
-    {
-      name: "routes.ts",
-      type: "file",
-      path: ["src"],
-    },
+      return (
+        dirent.isFile() &&
+        (isDatabaseDir
+          ? dirent.parentPath.includes("_" + orm.toLowerCase())
+          : true) &&
+        (isEntitiesDir
+          ? dirent.parentPath.includes("_" + orm.toLowerCase())
+          : true)
+      );
+    })
+    .map((dirent) => {
+      const splitPath = dirent.parentPath.split("app_example\\");
+      const arrayPath = splitPath[1]?.split("\\");
+      return [...(arrayPath || []), dirent.name];
+    });
 
-    {
-      name: "example",
-      type: "folder",
-      path: ["src", "modules"],
-    },
-  ];
-
-  for (const file of files) {
-    copyFile(file);
+  for (const relativePath of dirFiles) {
+    copyFile(orm, relativePath);
   }
 }
 
-function copyFile(file: FileFolder) {
-  console.log("file", file);
-  const srcPath = path.join(appExamplePath, ...file.path, file.name);
+function copyFile(orm: ORM, relativePath: string[]) {
+  const srcPath = path.join(appExamplePath, ...relativePath);
+  const destPath = path
+    .join(appGeneratedPath, ...relativePath)
+    .replace("_" + orm.toLowerCase(), "");
 
-  if (file.type === "file") {
-    const destPath = path.join(appGeneratedPath, ...file.path, file.name);
-    console.log("srcPath", srcPath);
-    console.log("destPath", destPath);
-    fs.mkdirSync(path.dirname(destPath), { recursive: true });
-    return fs.copyFileSync(srcPath, destPath);
+  fs.mkdirSync(path.dirname(destPath), { recursive: true });
+  const exists = fs.existsSync(srcPath);
+  if (!exists) {
+    console.log("File does not exist", srcPath);
+    return;
   }
-
-  if (file.type === "folder") {
-    const dirFiles = fs.readdirSync(srcPath);
-    for (const dirFile of dirFiles) {
-      copyFile({
-        name: dirFile,
-        type: dirFile.includes(".") ? "file" : "folder",
-        path: [...file.path, file.name],
-      });
-    }
-  }
+  fs.copyFileSync(srcPath, destPath);
 }
